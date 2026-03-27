942930395b61bc5b32bf5d489132d8748bde4f21
Luiz Gagliardi <luizh_g@yahoo.com.br>
2026-03-27 10:36:24 -0300

    feat: add CardOverlayViewModel with show/hide state

---
+++ src/BazaarOverlay.WPF/ViewModels/CardOverlayViewModel.cs
@@ -0,0 +1,31 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace BazaarOverlay.WPF.ViewModels;
+
+public partial class CardOverlayViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private string? _cardUrl;
+
+    [ObservableProperty]
+    private bool _isVisible;
+
+    [ObservableProperty]
+    private double _left;
+
+    [ObservableProperty]
+    private double _top;
+
+    public void ShowCard(string url, double left, double top)
+    {
+        CardUrl = url;
+        Left = left;
+        Top = top;
+        IsVisible = true;
+    }
+
+    public void Hide()
+    {
+        IsVisible = false;
+    }
+}
---
+++ tests/BazaarOverlay.Tests/WPF/CardOverlayViewModelTests.cs
@@ -0,0 +1,48 @@
+using BazaarOverlay.WPF.ViewModels;
+using Shouldly;
+
+namespace BazaarOverlay.Tests.WPF;
+
+public class CardOverlayViewModelTests
+{
+    private readonly CardOverlayViewModel _vm = new();
+
+    [Fact]
+    public void InitialState_IsNotVisible()
+    {
+        _vm.IsVisible.ShouldBeFalse();
+        _vm.CardUrl.ShouldBeNull();
+    }
+
+    [Fact]
+    public void ShowCard_SetsUrlAndVisibility()
+    {
+        _vm.ShowCard("https://bazaardb.gg/items/pigomorph", 100, 200);
+
+        _vm.IsVisible.ShouldBeTrue();
+        _vm.CardUrl.ShouldBe("https://bazaardb.gg/items/pigomorph");
+        _vm.Left.ShouldBe(100);
+        _vm.Top.ShouldBe(200);
+    }
+
+    [Fact]
+    public void Hide_ClearsVisibility()
+    {
+        _vm.ShowCard("https://bazaardb.gg/items/pigomorph", 100, 200);
+        _vm.Hide();
+
+        _vm.IsVisible.ShouldBeFalse();
+    }
+
+    [Fact]
+    public void ShowCard_WhileAlreadyShowing_UpdatesUrl()
+    {
+        _vm.ShowCard("https://bazaardb.gg/items/pigomorph", 100, 200);
+        _vm.ShowCard("https://bazaardb.gg/skills/fireball", 300, 400);
+
+        _vm.IsVisible.ShouldBeTrue();
+        _vm.CardUrl.ShouldBe("https://bazaardb.gg/skills/fireball");
+        _vm.Left.ShouldBe(300);
+        _vm.Top.ShouldBe(400);
+    }
+}
